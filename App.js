7b7ee638ed99be769ab24ffc8b94cbf5c34c6075
syukrontanzilah <syukrontanzilah@gmail.com>
2020-09-04 08:13:30 +0700

    add package

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>shop app</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
